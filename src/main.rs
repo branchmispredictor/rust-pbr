16b260aaf83f5e744e4a81f94db126e9954d4875
branchmispredictor <36093392+branchmispredictor@users.noreply.github.com>
2018-02-03 08:40:01 -0500

    PPM Image Code

---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use std::fs::File;
+use std::io::Write;
+use std::error::Error;
+
+
+struct PPM {
+    width: u32,
+    height: u32,
+    buffer: Box<[u8]>,
+}
+
+#[derive(Debug)]
+struct RGB(u8, u8, u8);
+
+impl PPM {
+    fn new(width: u32, height: u32) -> PPM {
+        let size = (3 * width * height) as usize;
+        let buffer: Box<[u8]> = vec![0; size].into_boxed_slice();
+        PPM {width, height, buffer}
+    }
+
+    fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    fn offset(&self, x: u32, y: u32) -> usize {
+        let offset = (y * self.width + x) * 3;
+        offset as usize
+    }
+
+    fn set(&mut self, x: u32, y: u32, color: RGB) {
+        let offset = self.offset(x, y);
+        self.buffer[offset] = color.0;
+        self.buffer[offset + 1] = color.1;
+        self.buffer[offset + 2] = color.2;
+    }
+
+    fn get(&self, x: u32, y: u32) -> RGB {
+        let offset = self.offset(x, y);
+        let r = self.buffer[offset];
+        let g = self.buffer[offset + 1];
+        let b = self.buffer[offset + 2];
+        RGB(r, g, b)
+    }
+
+    fn save(&self, filename: &str) -> std::io::Result<()> {
+        let mut file = try!(File::create(filename));
+        let header = format!("P6 {} {} 255\n", self.width, self.height);
+        try!(file.write(header.as_bytes()));
+        try!(file.write(&self.buffer));
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+impl Vector3 {
+    fn length(&self) -> f64 {
+        let &Vector3 {x, y, z} = self;
+        (x * x + y * y + z * z).sqrt()
+    }
+
+    fn normalize(&self) -> Vector3 {
+        let &Vector3 {x, y, z} = self;
+        let len = self.length();
+        v3(x / len, y / len, z / len)
+    }
+}
+
+struct Vector4 {
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+}
+
+impl Vector4 {
+    fn length(&self) -> f64 {
+        let &Vector4 {x, y, z, w} = self;
+        (x * x + y * y + z * z + w * w).sqrt()
+    }
+
+    fn normalize(&self) -> Vector4 {
+        let &Vector4 {x, y, z, w} = self;
+        let len = self.length();
+        v4(x / len, y / len, z / len, w / len)
+    }
+}
+
+fn v3(x: f64, y: f64, z: f64) -> Vector3 {
+    Vector3 {x, y, z}
+}
+
+fn v4(x: f64, y: f64, z: f64, w: f64) -> Vector4 {
+    Vector4 {x, y, z, w}
+}
+
+fn main() {
+    let width = 1280;
+    let height = 720;
+
+    let mut ppm = PPM::new(width, height);
+
+    for x in 0..width {
+        for y in 0..height {
+            let v = v3(
+                (x as f64) / (width as f64),
+                (y as f64) / (height as f64),
+                0.2,
+            );
+
+            let pix = RGB(
+                (v.x * 255.0) as u8,
+                (v.y * 255.0) as u8,
+                (v.z * 255.0) as u8,
+            );
+
+
+            ppm.set(x, y, pix);
+        }
+    }
+
+    match ppm.save("test.ppm") {
+        Err(why) => println!("Error: couldn't save ppm: {}", why.description()),
+        Ok(_) => ()
+    };
+}
